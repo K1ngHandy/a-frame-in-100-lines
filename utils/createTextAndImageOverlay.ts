4b8b535fd58ca9d5af826b13ae64ad3741f420c4
K1ngHandy <bolds-slicks0g@icloud.com>
2024-02-25 12:33:38 -0500

    modify text and image overlay

--- utils/createTextAndImageOverlay.ts
+++ utils/createTextAndImageOverlay.ts
@@ -5,4 +5,18 @@ enum Currency {
 
 export const createTextAndImageOverlay = async (currency: Currency) => {
   const apiKeyToken = process.env.ETHERSCAN;
+
+  const url = `https://api.etherscan.io/api?module=stats&action=ethprice&apikey=${apiKeyToken}`;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch ETH price');
+    }
+
+    const data = await response.json();
+
+    const textCurrency = currency === Currency.USD ? data.result.ethusd : data.result.ethbtc;
+  }
 };
